9a56c463eddada79c9675854cb9627c40dc42312
Mio-ctrl <158206744+Mio-ctrl@users.noreply.github.com>
2025-05-30 14:45:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from routers import inserate, inserat
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     version="1.0.0"
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Erlaubt alle Origins - für Produktion spezifischer machen
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return {
